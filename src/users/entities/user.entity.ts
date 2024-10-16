62232196f4ee2f4f34a4713c6ce64999d4311e89
daniel.xpert <daniel.otero@xpertgroup.co>
2024-10-15 21:41:17 -0500

    new collection

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({ unique: true, required: true })
+  email: string;
+  @Prop({ required: true, minlength: 4 })
+  password: string;
+  @Prop({ required: true })
+  name: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
